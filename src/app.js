892550a9fe1de40e449d9a555ac6c7a3f404f6bb
Anders Wood <anderswood@gmail.com>
2017-07-04 16:34:33 -0600

    update app directory path

--- src/app.js
+++ src/app.js
@@ -25,12 +25,12 @@ if (process.env.NODE_ENV !== 'production') {
   }));
 }
 
-app.use('/assets', express.static(path.join(__dirname, '../public')));
+app.use('/', express.static(path.join(__dirname, '../public')));
 
-app.get('/', function (req, res) { res.sendFile(path.join(__dirname, '/../public/index.html')) });
+app.get('/', function (req, res) { res.sendFile(path.join(__dirname, '../public/index.html')) });
 
 app.use('/api', router);
-app.get('/*', function (req, res) { res.sendFile(path.join(__dirname, '/../public/index.html')) });
+app.get('/*', function (req, res) { res.sendFile(path.join(__dirname, '../public/index.html')) });
 
 app.listen(port);
 
